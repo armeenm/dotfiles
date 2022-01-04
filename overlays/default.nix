fbc5f6b0c05e07ca4ac08a7d66150ad480b95acd
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-01-04 09:01:37 -0600

    Remove seafile-shared overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,6 @@ final: prev: {
   mdadm = prev.callPackage ./mdadm { pkgs = prev; };
   nixUnstable = prev.callPackage ./nixUnstable { pkgs = prev; };
   profanity = prev.callPackage ./profanity { pkgs = prev; };
-  seafile-shared = prev.callPackage ./seafile-shared { pkgs = prev; };
   swtpm = prev.callPackage ./swtpm { pkgs = prev; };
   whatsapp-for-linux = prev.callPackage ./whatsapp-for-linux { pkgs = prev; };
 
@@ -15,6 +14,5 @@ final: prev: {
 
   go = prev.go.overrideAttrs (_: {
     doCheck = false;
-    checkPhase = "";
   });
 }
--- overlays/seafile-shared/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }:
-
-pkgs.seafile-shared.overrideAttrs (old: {
-  pythonPath = old.pythonPath ++ [ pkgs.python3.pkgs.future ];
-})
