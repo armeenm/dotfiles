f807ed3ca740f4ed01e49f7f49dfaba3b00e5a3e
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-01-05 10:53:57 -0600

    Delete ext/

--- ext/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-rec {
-  toAttrPath = base: rest:
-    if lib.isString rest
-    then toAttrPath (lib.splitString "." rest)
-    else if lib.isList rest
-    then builtins.foldl' (lib.flip builtins.getAttr) base rest
-    else null;
-}
