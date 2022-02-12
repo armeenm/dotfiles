b5b669fa762650c299fee101541a0a1c06bd0151
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-02-11 22:05:47 -0600

    clean up overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,67 +1,11 @@
 final: prev: {
   discord-canary = prev.callPackage ./discord-canary { pkgs = prev; };
   mathematica = prev.callPackage ./mathematica { pkgs = prev; };
-  mdadm = prev.callPackage ./mdadm { pkgs = prev; };
   nixUnstable = prev.callPackage ./nixUnstable { pkgs = prev; };
-  whatsapp-for-linux = prev.callPackage ./whatsapp-for-linux { pkgs = prev; };
 
   elfutils = prev.elfutils.overrideAttrs (_: {
     doCheck = false;
     doInstallCheck = false;
-  });
-
-  ddclient = prev.ddclient.overrideAttrs (old: rec {
-    src = prev.fetchFromGitHub {
-      owner = "ddclient";
-      repo = "ddclient";
-      rev = "c29919bb0fb61eecbb7a9e02a8555fa9785f9b0a";
-      hash = "sha256-nvuWyCJPe7yBxwDhUpNSevY+T3pFwA10xsjIG9y0/Aw=";
-    };
-
-    propagatedBuildInputs = old.buildInputs ++ (with prev.perlPackages; [
-      TestMockModule
-      TestTCP
-      TestWarnings
-      URI
-    ]);
-
-    nativeBuildInputs = [ prev.autoreconfHook ];
-
-    preConfigure = "touch Makefile.PL";
-    
-    buildPhase = ''
-      runHook preBuild
-
-      mkdir -p $out/bin
-      touch $out/bin/what5
-      make -j $NIX_BUILD_CORES
-
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      perlFlags=$(head -n1 ddclient.in)
-      perlFlags=''${perlFlags#* }
-      sed -i "1s|$| $perlFlags|" ddclient
-
-      make install
-
-      runHook postInstall
-    '';
-
-    dontFixup = true;
-    
-    #fixupPhase = "";
-
-    #postFixup = ''
-    #  wrapProgram "$out/bin/ddclient" --prefix PERL5LIB : "${prev.perlPackages.makePerlPath propagatedBuildInputs}"
-    #'';
-
-    doCheck = false;
-
-    checkPhase = "make VERBOSE=1 check";
   });
 
   wlroots = prev.wlroots.overrideAttrs (_: rec {
--- overlays/whatsapp-for-linux/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }:
-
-pkgs.whatsapp-for-linux.overrideAttrs (old: {
-  buildInputs = old.buildInputs ++ [ pkgs.glib-networking ];
-})
