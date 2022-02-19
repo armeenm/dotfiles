89e54c26e44d9d182a721d8ba32608105d6b33f4
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-02-19 13:15:49 -0600

    add ldap

---
+++ hosts/lithium/auth.nix
@@ -0,0 +1,52 @@
+{ config, lib, domain, ... }:
+
+let
+  openldap = config.services.openldap.package;
+  dbDir = "/var/db/ldap";
+  suffix = "dc=armeen,dc=org";
+in {
+  services = {
+    openldap = {
+      enable = true;
+      urlList = [ "ldap:///" ];
+      settings = {
+        attrs.olcLogLevel = [ "stats" ];
+        children = {
+          "cn=schema".includes = [
+            "${openldap}/etc/schema/core.ldif"
+            "${openldap}/etc/schema/cosine.ldif"
+            "${openldap}/etc/schema/inetorgperson.ldif"
+          ];
+
+          "olcDatabase={-1}frontend".attrs = {
+            objectClass = "olcDatabaseConfig";
+            olcDatabase = "{-1}frontend";
+            olcAccess = [
+              "{0}to * by dn.exact=uidNumber=0+gidNumber=0,cn=peercred,cn=external,cn=auth manage stop by & none stop"
+            ];
+          };
+
+          "olcDatabase={0}config".attrs = {
+            objectClass = "olcDatabaseConfig";
+            olcDatabase = "{0}config";
+            olcAccess = [ "{0}to * by * none break" ];
+          };
+
+          "olcDatabase={1}mdb".attrs = {
+            objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
+            olcDatabase = "{1}mdb";
+            olcDbDirectory = dbDir;
+            olcDbIndex = [
+              "objectClass eq"
+              "uid pres,eq"
+              "cn pres,eq"
+              "sn pres,eq,subany"
+            ];
+            olcSuffix = suffix;
+            olcAccess = [ "{0}to * by * read break" ];
+          };
+        };
+      };
+    };
+  };
+}
