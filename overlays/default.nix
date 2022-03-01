8c8d25a5482bbeb1fc5c497bc92d869ac5efc5d1
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-03-01 08:21:17 -0600

    switch to nixos-unstable's nixUnstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,6 @@
 final: prev: {
   discord-canary = prev.callPackage ./discord-canary { pkgs = prev; };
   mathematica = prev.callPackage ./mathematica { pkgs = prev; };
-  nixUnstable = prev.callPackage ./nixUnstable { pkgs = prev; };
 
   elfutils = prev.elfutils.overrideAttrs (_: {
     doCheck = false;
