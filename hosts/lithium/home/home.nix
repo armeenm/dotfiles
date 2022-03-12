a45ae6cf09b66baa627c8a81620df25a77387486
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-03-11 20:08:40 -0600

    add a couple of commented-out wlroots-related envars
      use these maybe if graphics are messed up
      some claim these fix flickering with nvidia prop. drivs; no luck

--- hosts/lithium/home/home.nix
+++ hosts/lithium/home/home.nix
@@ -246,6 +246,8 @@ in
     MOZ_ENABLE_WAYLAND = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
     XKB_DEFAULT_OPTIONS = "caps:escape";
+    #WLR_DRM_NO_ATOMIC = "1";
+    #WLR_DRM_NO_MODIFIERS = "1";
     #NIXOS_OZONE_WL = "1";
 
     # Cleaning up home dir
