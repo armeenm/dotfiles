7a328b2ff686fa65ab3875e17f71fa2b013c1b32
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-04-17 17:59:42 -0500

    chore: more stupid formatting fixes from a long time ago

--- conf/emacs/default.el
+++ conf/emacs/default.el
@@ -6,32 +6,32 @@
 (load-theme 'gruvbox-dark-hard t)
 
 (setq
-menu-bar-mode nil
-tool-bar-mode nil
-scroll-bar-mode nil
-global-hl-line-mode nil
-column-number-mode t
-xterm-mouse-mode t
-save-place-mode t
-visible-bell t)
+ menu-bar-mode nil
+ tool-bar-mode nil
+ scroll-bar-mode nil
+ global-hl-line-mode nil
+ column-number-mode t
+ xterm-mouse-mode t
+ save-place-mode t
+ visible-bell t)
 
 (global-display-line-numbers-mode)
 (frames-only-mode)
 
 ;; code ;;
 (setq
-  standard-indent 2
-  indent-line-function 'insert-tab)
+ standard-indent 2
+ indent-line-function 'insert-tab)
 
 (setq-default
-  indent-tabs-mode nil
-  tab-width 2)
+ indent-tabs-mode nil
+ tab-width 2)
 
 (setq
-js-indent-level 2
-c-default-style "k&r"
-c-basic-offset 2
-verilog-auto-newline nil)
+ js-indent-level 2
+ c-default-style "k&r"
+ c-basic-offset 2
+ verilog-auto-newline nil)
 
 (set-default 'semantic-case-fold t)
 
@@ -39,72 +39,73 @@ verilog-auto-newline nil)
 
 ;; backups ;;
 (if (not (file-exists-p --backup-directory))
-(make-directory --backup-directory t))
+    (make-directory --backup-directory t))
 (setq backup-directory-alist `(("." . ,--backup-directory)))
 (setq make-backup-files t
-backup-by-copying t
-version-control t
-delete-old-versions t
-delete-by-moving-to-trash t
-kept-old-versions 6
-kept-new-versions 9
-auto-save-default t
-auto-save-timeout 20
-auto-save-interval 200)
+      backup-by-copying t
+      version-control t
+      delete-old-versions t
+      delete-by-moving-to-trash t
+      kept-old-versions 6
+      kept-new-versions 9
+      auto-save-default t
+      auto-save-timeout 20
+      auto-save-interval 200)
 
 ;; evil ;;
-(setq   evil-want-keybinding nil
-evil-want-Y-yank-to-eol t
-evil-search-wrap t
-evil-regexp-search t
-evil-undo-system 'undo-fu)
+(setq
+ evil-want-keybinding nil
+ evil-want-Y-yank-to-eol t
+ evil-search-wrap t
+ evil-regexp-search t
+ evil-undo-system 'undo-fu)
 (evil-mode)
 (evil-collection-init)
 
 ;; general.el ;;
 (general-create-definer my-leader-def
-:prefix "SPC")
+  :prefix "SPC")
 
 (general-create-definer my-local-leader-def
-:prefix "SPC m")
+  :prefix "SPC m")
 
 (general-evil-setup)
 
 (my-local-leader-def
-:states 'motion
-:keymaps 'override
-"SPC" 'save-buffer
-"c" 'counsel-git
-"g" 'magit
-"l" 'lsp-command-map
-"r" 'ivy-resume
-"w" 'evil-window-map
-"p" 'projectile-command-map
-"b b" 'ivy-switch-buffer
-"b e" 'eval-buffer
-"b k" 'kill-buffer
-"b l" 'list-buffers
-"t i" 'ivy-mode
-"/ a" 'counsel-ag
-"/ c" 'avy-goto-char-2
-"/ f" 'find-file
-"/ g" 'counsel-git-grep
-"/ l" 'find-library
-"k f" 'describe-function
-"k s" 'describe-symbol
-"k v" 'describe-variable
-"x m" 'lsp-ui-imenu)
+ :states 'motion
+ :keymaps 'override
+ "SPC" 'save-buffer
+ "c" 'counsel-git
+ "g" 'magit
+ "l" 'lsp-command-map
+ "r" 'ivy-resume
+ "w" 'evil-window-map
+ "p" 'projectile-command-map
+ "b b" 'ivy-switch-buffer
+ "b e" 'eval-buffer
+ "b k" 'kill-buffer
+ "b l" 'list-buffers
+ "t i" 'ivy-mode
+ "/ a" 'counsel-ag
+ "/ c" 'avy-goto-char-2
+ "/ f" 'find-file
+ "/ g" 'counsel-git-grep
+ "/ l" 'find-library
+ "k f" 'describe-function
+ "k s" 'describe-symbol
+ "k v" 'describe-variable
+ "x m" 'lsp-ui-imenu)
 
 (general-define-key
-"M-/" 'avy-goto-char)
+ "M-/" 'avy-goto-char)
 
 (general-def 'normal
-"u" 'undo-fu-only-undo
-"C-r" 'undo-fu-only-redo
-"/" 'swiper)
+  "u" 'undo-fu-only-undo
+  "C-r" 'undo-fu-only-redo
+  "/" 'swiper)
 
 (general-def 'normal lsp-mode-map
-"K" 'lsp-describe-thing-at-point)
+  "K" 'lsp-describe-thing-at-point)
 
 ;; swiper/ivy/counsel ;;
 (ivy-count-format "(%d/%d) ")
@@ -112,11 +113,11 @@ evil-undo-system 'undo-fu)
 (enable-recursive-minibuffers t)
 
 (ivy-virtual-abbreviate 'full
-ivy-rich-switch-buffer-align-virtual-buffer t
-ivy-rich-path-style 'abbrev)
+                        ivy-rich-switch-buffer-align-virtual-buffer t
+                        ivy-rich-path-style 'abbrev)
 
 (ivy-set-display-transformer 'ivy-switch-buffer
-'ivy-rich-switch-buffer-transformer)
+                             'ivy-rich-switch-buffer-transformer)
 
 (counsel-mode)
 (ivy-mode)
