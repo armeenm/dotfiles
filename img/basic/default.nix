bff8aacfd3ef6fcda2bee2b47c948d777dcd0ef3
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-06-06 16:53:15 -0500

    feat(img/basic): add useful utilities

--- img/basic/default.nix
+++ img/basic/default.nix
@@ -9,10 +9,18 @@
     networkmanager.enable = true;
   };
 
+  console = {
+    keyMap = "us";
+    font = "Tamsyn7x13r";
+    packages = [ pkgs.tamsyn ];
+    earlySetup = false;
+  };
+
   nix = {
     package = pkgs.nixUnstable;
     extraOptions = ''
-      experimental-features = nix-command flakes
+      warn-dirty = false
+      experimental-features = nix-command flakes ca-derivations
     '';
   };
 
@@ -21,12 +29,38 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
+      defaultEditor = true;
+      configure = {
+        customRC = ''
+          set number
+          set hidden
+          set shell=bash
+          set cmdheight=2
+          set nocompatible
+          set shortmess+=c
+          set updatetime=300
+          set background=dark
+          set foldmethod=marker
+          set signcolumn=yes
+          set nobackup nowritebackup
+          set tabstop=2 shiftwidth=2 expandtab
+          set tagrelative
+          set tags^=./.git/tags;
+          set mouse=a
+        '';
+      };
     };
   };
 
   environment.systemPackages = with pkgs; [
     btop
     git
+    hdparm
     htop
+    lm_sensors
+    lshw
+    pciutils
+    smartmontools
+    usbutils
   ];
 }
