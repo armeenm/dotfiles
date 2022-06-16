f99987a0ec37c614a1d4dc945ed428a6a0047677
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-06-16 07:27:32 -0500

    chore: bump

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,22 +2,13 @@ final: prev: {
   #discord-canary = prev.callPackage ./discord-canary { pkgs = prev; };
   #mathematica = prev.callPackage ./mathematica { pkgs = prev; };
 
-  #elfutils = prev.elfutils.overrideAttrs (_: {
-  #  doCheck = false;
-  #  doInstallCheck = false;
-  #});
+  cryptsetup = prev.cryptsetup.overrideAttrs (_: {
+    doCheck = false;
+    doInstallCheck = false;
+  });
 
-  #libpulseaudio = prev.libpulseaudio.overrideAttrs (_: {
-  #  doCheck = false;
-  #  doInstallCheck = false;
-  #});
-
-  #pulseaudio = prev.pulseaudio.overrideAttrs (_: {
-  #  doCheck = false;
-  #  doInstallCheck = false;
-  #});
-
-  #wlroots = prev.wlroots.overrideAttrs (old: {
-  #  buildInputs = old.buildInputs ++ [ prev.vulkan-headers ];
-  #});
+  dconf = prev.dconf.overrideAttrs (_: {
+    doCheck = false;
+    doInstallCheck = false;
+  });
 }
