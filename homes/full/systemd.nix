87575f84bc6a2d8d0c02d2aa7e6e975d50e8444a
Armeen Mahdian <mahdianarmeen@gmail.com>
2022-10-16 16:25:54 -0500

    feat: add preliminary systemd hyprland support

--- homes/full/systemd.nix
+++ homes/full/systemd.nix
@@ -1,3 +1,22 @@
-{ config, pkgs, lib, root, user, ... }:
+{ config, sys, pkgs, lib, root, user, ... }:
 
-{ }
+{
+  user = {
+    startServices = "sd-switch";
+
+    services = {
+      hyprland = {
+        Unit = {
+          Description = "A dynamic tiling Wayland compositor that doesn't sacrifice on its looks.";
+          Documentation = "https://wiki.hyprland.org";
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          Environment = [ "PATH=${lib.makeBinPath (with pkgs; [ bash coreutils gnugrep pciutils ])}" ];
+          ExecStart = "${sys.programs.hyprland.package}/bin/Hyprland";
+        };
+      };
+    };
+  };
+}
