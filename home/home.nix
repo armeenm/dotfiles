977c386237edb8d14a7d14143726ff0c44ae6722
Armeen Mahdian <armeen@fulminous-hill.com>
2025-03-15 13:04:00 -0700

    chore: bump flakes

--- home/home.nix
+++ home/home.nix
@@ -35,9 +35,6 @@ in
     username = lib.mkOverride 500 user.login;
     homeDirectory = lib.mkOverride 500 "/home/${user.login}";
 
-    # XXX: https://github.com/nix-community/home-manager/issues/4826
-    activation.batCache = lib.mkForce (lib.hm.dag.entryAfter [ "linkGeneration" ] '''');
-
     packages = with pkgs; [
       adwaita-icon-theme
       age-plugin-yubikey
