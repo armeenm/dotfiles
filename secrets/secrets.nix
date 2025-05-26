933c2499057603a596fd419f905c5ba703ada063
Armeen Mahdian <armeen@fulminous-hill.com>
2025-05-25 18:41:21 -0700

    chore: rekeying

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   armeen-yk0 = "age1yubikey1qvdrue6hh6hnrvn0md3txjg0p6rv504ujhkkta0tw8dzuh69rlwtgwjfng2";
 
-  argentum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIV5dC9ul2rIbO/GPlOS0gT7u8dUpDW4hLEFu0sDij5w";
+  argentum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxDNCxl2g31OXyVTRdWGr+fv9/LTmhHxloQ6or9NTpR";
   lithium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJl6/aiWgReQ80Tbde4fLE1vdM49alVEREXnUIN5NDVP";
   carbon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIy8r+XjFmIpZFDTnJxJ8mhqXnayILGziupst+1RFYtA";
   hosts = [ argentum lithium carbon ];
